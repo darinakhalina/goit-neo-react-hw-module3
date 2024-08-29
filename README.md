320ea64250d7babe8c1abd0797c782798e0d5ffe
Darina Khalina <darina.khalina@namecheap.com>
2024-08-29 21:38:44 +0300

    add ContactForm component

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Link -
+Demo link - https://goit-neo-react-hw-module3-git-main-darina-khalinas-projects.vercel.app/
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,42 @@
+import { useState, useEffect } from 'react';
+import ContactForm from '../ContactForm/ContactForm';
 import css from './App.module.css';
 
+const defaultContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+const localStorageKey = 'contacts';
+
 function App() {
-  return <div className={css.app}>Test</div>;
+  const [contacts, setContacts] = useState(() => {
+    const localStorageItem = localStorage.getItem(localStorageKey);
+    return localStorageItem ? JSON.parse(localStorageItem) : defaultContacts;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = ({ name, number }) => {
+    const newContact = {
+      id: `id-${Date.now().toString(36)}`,
+      name,
+      number,
+    };
+
+    setContacts(prevState => [...prevState, newContact]);
+  };
+
+  return (
+    <div className={css.app}>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+    </div>
+  );
 }
 
 export default App;
